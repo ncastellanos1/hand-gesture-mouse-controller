1fc163ef6ad0633ced3f86b441037d750005d43d
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2024-01-15 23:52:12 -0500

    feat: clicks

--- src/controller/mouse_controller.py
+++ src/controller/mouse_controller.py
@@ -18,18 +18,28 @@ class MouseController:
         self.click_delay = 0.5  # 500 ms delay between clicks
         self.smooth_factor = 0.5
         self.last_mouse_pos = (0, 0)
+        self.last_hand_landmarks = None
 
     def move_mouse(self, hand_landmarks, frame_shape):
-        """Move mouse based on hand landmarks"""
-        x, y = int(hand_landmarks.landmark[8].x * frame_shape[1]), int(hand_landmarks.landmark[8].y * frame_shape[0])
-        x_scaled = np.interp(x, [0, frame_shape[1]], [0, self.ancho_total])
-        y_scaled = np.interp(y, [0, frame_shape[0]], [0, self.alto_max])
+        """Move mouse based on hand landmarks, inverting horizontal movement and incorporating hand speed."""
+        current_x, current_y = int(hand_landmarks.landmark[8].y * frame_shape[0]), int(hand_landmarks.landmark[8].x * frame_shape[1])
 
-        smoothed_x = self.last_mouse_pos[0] * (1 - self.smooth_factor) + x_scaled * self.smooth_factor
-        smoothed_y = self.last_mouse_pos[1] * (1 - self.smooth_factor) + y_scaled * self.smooth_factor
+        if self.last_hand_landmarks:
+            last_x, last_y = int(self.last_hand_landmarks.landmark[8].y * frame_shape[0]), int(self.last_hand_landmarks.landmark[8].x * frame_shape[1])
+            velocity_x = current_x - last_x
+            velocity_y = current_y - last_y
+        else:
+            velocity_x, velocity_y = 0, 0
+
+        x_scaled = np.interp(current_x + velocity_x, [0, frame_shape[0]], [0, self.alto_max])
+        y_scaled = np.interp(current_y + velocity_y, [0, frame_shape[1]], [self.ancho_total, 0])
+
+        smoothed_x = self.last_mouse_pos[1] * (1 - self.smooth_factor) + y_scaled * self.smooth_factor
+        smoothed_y = self.last_mouse_pos[0] * (1 - self.smooth_factor) + x_scaled * self.smooth_factor
 
         pyautogui.moveTo(smoothed_x, smoothed_y)
-        self.last_mouse_pos = (smoothed_x, smoothed_y)
+        self.last_mouse_pos = (smoothed_y, smoothed_x)
+        self.last_hand_landmarks = hand_landmarks
 
     def click(self, direction):
         """Click"""
--- src/main.py
+++ src/main.py
@@ -9,8 +9,6 @@ def main():
     Main function.
     """
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 260)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
     hand_detector = HandDetector()
     head_orientation_detector = HeadOrientationDetector()
 
@@ -22,21 +20,22 @@ def main():
         if not ret:
             continue
 
-        current_head_orientation = head_orientation_detector.get_head_orientation(frame)
-
-        if current_head_orientation != head_orientation_detector.last_head_orientation:
-            target_monitor = multi_monitor_controller.determine_target_monitor(current_head_orientation)
-            multi_monitor_controller.move_mouse_to_monitor(target_monitor)
+        # current_head_orientation = head_orientation_detector.get_head_orientation(frame)
+        #
+        # if current_head_orientation != head_orientation_detector.last_head_orientation:
+        #     target_monitor = multi_monitor_controller.determine_target_monitor(current_head_orientation)
+        #     multi_monitor_controller.move_mouse_to_monitor(target_monitor)
 
         results = hand_detector.process_frame(frame)
 
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                if hand_detector.is_index_finger_raised(hand_landmarks.landmark):
-                    mouse_controller.move_mouse(hand_landmarks, frame.shape)
-                if hand_detector.is_right_click(hand_landmarks.landmark):
-                    mouse_controller.click(hand_detector.RIGHT)
+
                 if hand_detector.is_hand_closed(hand_landmarks.landmark):
+                    mouse_controller.move_mouse(hand_landmarks, frame.shape)
+                elif hand_detector.is_index_finger_raised(hand_landmarks.landmark):
+                    mouse_controller.click(hand_detector.LEFT)
+                elif hand_detector.is_right_click(hand_landmarks.landmark):
                     mouse_controller.click(hand_detector.LEFT)
 
         cv2.imshow('MediaPipe Hands', frame)
